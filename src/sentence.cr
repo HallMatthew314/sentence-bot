9d565f8ed563cff4fb237e40b319229d7f2a65f9
Matthew Hall <hallmatthew314@gmail.com>
2021-10-06 14:42:49 +1300

    Variables now work

--- src/sentence.cr
+++ src/sentence.cr
@@ -3,6 +3,18 @@ class Sentence
   end
 
   def substitue(category_set : Hash(String, FileList)) : String
-    @text.gsub(/%(.+?)%/) { |x| category_set[x[1, x.size - 2]].sample }
+    # Create a mapping each each variable category
+    variables = {} of String => String
+    @text.scan(/(?<=%)(\?.+?)(?=%)/) do |x|
+      s = x[1]
+      variables[s] = category_set.keys.sample unless variables.has_key?(s)
+    end
+
+    # Perform substitution
+    @text.gsub(/%.+?%/) do |x|
+      x = (x[0] == '%' ? x[1, x.size - 2] : x)
+      index = variables.fetch(x, x)
+      category_set[index].sample
+    end
   end
 end
